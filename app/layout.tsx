77b1e68c1c3d9871a6bc6a63a2fc6fd02cd76124
adrianmross <adrian.ross@oracle.com>
2026-03-07 18:04:12 -0800

    feat: add Vercel Speed Insights to app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Syne, Geist_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const syne = Syne({
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${syne.variable} ${geistMono.variable} font-sans antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
